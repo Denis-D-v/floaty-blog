1679f8884cf5ba1c0c31807400fcf71c701c72aa
EnshteinDe <sonikovskiy@gmail.com>
2024-09-12 01:38:18 +0300

    Added Delete User api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,3 +53,17 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  // console.log('User ID from token:', req.user.id);
+  // console.log('User ID from params:', req.params.userId);
+
+  if (req.user.id !== req.params.userId)
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
