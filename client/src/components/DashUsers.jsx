927d15fa2275522409a23cd12353220108df02ef
EnshteinDe <sonikovskiy@gmail.com>
2024-10-28 01:05:38 +0300

    complete Post Page Functionality

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -70,7 +70,7 @@ export default function DashUsers() {
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && users.length > 0 ? (
         <>
-          <Table className="shadow-md table-fixed w-full">
+          <Table className="shadow-md ">
             <Table.Head>
               <Table.HeadCell>Date created</Table.HeadCell>
               <Table.HeadCell>User image</Table.HeadCell>
