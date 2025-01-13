d4c3ba26201ef48f71a9875448591fa9658bd84f
EnshteinDe <sonikovskiy@gmail.com>
2025-01-13 21:25:38 +0300

    Fix require in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     './index.html',
@@ -8,5 +11,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar'), ],
+  plugins: ['flowbite/plugin', 'tailwind-scrollbar'],
 };
